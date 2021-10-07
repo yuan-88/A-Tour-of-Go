80733e8e540e6546173f82d3ad4060dfa4382b35
yuan <idzhanghao@outlook.com>
2021-10-07 23:29:34 +0900

    Add equivalent binary tree

---
+++ exercise-equivalent-binary-trees.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/tour/tree"
+)
+
+// Walk 步进 tree t 将所有的值从 tree 发送到 channel ch。
+func Walk(t *tree.Tree, ch chan int) {
+	walk(t, ch)
+	close(ch)
+}
+
+func walk(t *tree.Tree, ch chan int) {
+	if t == nil {
+		return
+	}
+	walk(t.Left, ch)
+	ch <- t.Value
+	walk(t.Right, ch)
+}
+
+// Same 检测树 t1 和 t2 是否含有相同的值。
+func Same(t1, t2 *tree.Tree) bool {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
+	for {
+		v1, ok1 := <-ch1
+		v2, ok2 := <-ch2
+		switch {
+		case !ok1, !ok2:
+			return ok1 == ok2
+		case v1 != v2:
+			return false
+		}
+	}
+}
+
+func main() {
+	ch := make(chan int)
+	go Walk(tree.New(1), ch)
+	
+	for {
+		v, ok := <-ch
+		if !ok {
+			break
+		}
+		fmt.Println(v)
+	}
+		
+	fmt.Println(Same(tree.New(1), tree.New(1)))
+	fmt.Println(Same(tree.New(1), tree.New(2)))
+}
